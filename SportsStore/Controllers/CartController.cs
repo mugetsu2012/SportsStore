78af7b2d8f9c940e810e20e0b50094a6edde4c2b
Douglas Ortiz <alexanderortiz333@gmail.com>
2018-05-28 22:10:23 -0600

    Me quedo en pagina 290, listar cart

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class CartController : Controller
     {
-        [HttpPost]
-        public IActionResult AddToCart()
+        private readonly IProductsRepository _productsRepository;
+
+        public CartController(IProductsRepository productsRepository)
         {
-            return View();
+            _productsRepository = productsRepository;
+        }
+
+        [HttpPost]
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _productsRepository.Products.FirstOrDefault(x => x.ProductId == productId);
+
+            if (product != null)
+            {
+                Cart cart = new Cart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _productsRepository.Products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
